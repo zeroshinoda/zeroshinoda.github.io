638d9180c626495328f2c7e868f75a8ff8c86410
zeroshinoda <zeroshinoda4391@gmail.com>
2025-12-19 14:39:54 +0700

    make it app-like

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'bunny-kanji-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './joyo_kanji_final.csv',
+  './manifest.json',
+  './icon_big.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
